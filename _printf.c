6f38ed8e91cdbbdf5afc8c175d8b8fd3541abfca
oussama laamri <laamri2002@gmail.com>
2023-04-23 16:54:09 +0000

    i add file _printf.c and i write tow cases case of (char) and case of (%)

---
+++ _printf.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * _printf - function that produces output like real printf
+ *
+ * @format:  is a character string
+ * Return: c
+ */
+int _printf(const char *format, ...)
+{
+	va_list osarg;
+	int c = 0;
+
+	va_start(osarg, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+				{
+					char character = va_arg(osarg, int);
+
+					putchar(character);
+					break;
+				}
+				case '%':
+				{
+					putchar('%');
+					break;
+				}
+
+			}
+		}
+		else
+		{
+			putchar(*format);
+			c++;
+		}
+		format++;
+	}
+	va_end(osarg);
+	return (c);
+}
