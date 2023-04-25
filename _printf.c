d2a7909067d3117257928159efe7c480896d646e
oussama laamri <laamri2002@gmail.com>
2023-04-25 15:20:27 +0000

    i make the code in a lot of func

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
-
+#include "main.h"
 /**
  * _printf - function that produces output like real printf
  *
@@ -12,7 +12,6 @@ int _printf(const char *format, ...)
 {
 	va_list osarg;
 	int c = 0;
-	int i;
 
 	if (format == NULL)
 		return (-1);
@@ -23,50 +22,11 @@ int _printf(const char *format, ...)
 		if (*format == '%')
 		{
 			format++;
-			switch (*format)
-			{
-			case 'c':
-			{
-				char character = va_arg(osarg, int);
-
-				write(STDOUT_FILENO, &character, 1);
-				c++;
-				break;
-			}
-			case '%':
-			{
-				write(STDOUT_FILENO, "%", 1);
-				c++;
-				break;
-			}
-			case 's':
-			{
-				char *string = va_arg(osarg, char*);
-
-				i = 0;
-				if (string == NULL)
-					string = "(null)";
-				while (string[i] != '\0')
-				{
-					write(STDOUT_FILENO, &string[i], 1);
-					i++;
-					c++;
-				}
-				break;
-			}
-			default:
-			{
-				write(STDOUT_FILENO, "%", 1);
-				write(STDOUT_FILENO, &*format, 1);
-				c = c + 2;
-				break;
-			}
-			}
+			c += switches(*format, osarg);
 		}
 		else
 		{
-			write(STDOUT_FILENO, format, 1);
-			c++;
+			c += print_character(*format);
 		}
 		format++;
 	}
--- main.h
+++ main.h
@@ -1,1 +1,6 @@
+#include <stdarg.h>
 int _printf(const char *format, ...);
+int print_character(char c);
+int print_string(char *str);
+int switches(char s, va_list osarg);
+
